4f0620504a4d572192d13331209eb928f95158fa
baranysf <baranrise@gmail.com>
2023-05-24 06:03:24 +0900

    proje tamam

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var index;
+
+
+console.log('index');
+
+
+
+
+
+
+
+
+
+
+
+for(index=0;index<10;index++){
+    document.getElementById('result').innerHTML += index + "<br>";
+}
+
+document.getElementById('result').style.color = "maroon";
+
